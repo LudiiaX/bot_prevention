c66943d617677039e31398a646728bb7492ffa20
21903121 <21903121@etu.unicaen.fr>
2023-03-01 01:42:34 +0100

    modification des messages, modifications du code pour qu'il n'envoi qu'un message, creation du .env proprement

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from discord.ext.commands import Bot
 import discord
 import os 
+# IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
+from dotenv import load_dotenv
+
+# IMPORT COMMANDS FROM THE DISCORD.EXT MODULE.
+from discord.ext import commands
+
+# LOADS THE .ENV FILE THAT RESIDES ON THE SAME LEVEL AS THE SCRIPT.
+load_dotenv()
+
+# GRAB THE API TOKEN FROM THE .ENV FILE.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents().all()
 bot = Bot("*", intents=intents)
 
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 #Amélioration :
 #Ne pas se prévenir soit meme quand on rejoint
+#Faire une liste de membre par serveur est non global
+#   avec un dico
 
 members = []
 list_members = []
@@ -20,32 +32,38 @@ async def on_ready():
     add_members()
     for i in members:
         await i.send("Bot pret")
-    await bot.change_presence(status = discord.Status.online, activity=discord.Game("rien"))
+    await bot.change_presence(status = discord.Status.online, activity=discord.Game("*help"))
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     global members
+    
     if after.channel != None:
         for i in members:
-            await i.send(str(member.name) + " vient de se connecter dans " + str(after.channel.name) + " du serveur " + str(after.channel.guild.name))
+            if str(after.channel.guild) == str(i.guild.name):
+                await i.send(str(member.name) + " vient de se connecter dans " + str(after.channel.name) + " du serveur " + str(after.channel.guild.name))
 
-@bot.command(name='add_me')
+@bot.command(name='add_me',
+             help="permet de s'ajouter du programme de prevention."
+)
 async def add_me(ctx):
     global list_members
     if ctx.author.name in list_members:
-        await ctx.send(f'{ctx.author.mention} est déjà dans programme de prévention.')
+        await ctx.send(f'{ctx.author.mention} tu es déjà dedans idiot.')
     else :
         list_members.append(ctx.author.name)
         add_members()
-        await ctx.send(f'{ctx.author.mention} a été ajouté au programme de prévention.')
+        await ctx.send(f'{ctx.author.mention} ouais je te pinguerais tkt.')
 
-@bot.command(name='delete_me')
-async def delete_me(ctx):
+@bot.command(name='remove_me',
+             help="permet de se retirer du programme de prevention."
+)
+async def remove_me(ctx):
     global list_members
     if ctx.author.name in list_members:
         list_members.remove(ctx.author.name)
         add_members()
-        await ctx.send(f'{ctx.author.mention} a été supprimé du programme de prévention.')
+        await ctx.send(f'{ctx.author.mention} ok c est bon je te spamerais plus tkt.')
 
 
 def add_members():
@@ -57,5 +75,5 @@ def add_members():
             members.append(i)
     
 print("Lancement du bot .....")
-bot.run(TOKEN)
+bot.run(DISCORD_TOKEN)
 
